26411aa7a62b6127c809c8ea6a641070520bc231
Hardik Sadhu <hardiksadhu472@gmail.com>
2024-03-09 12:13:47 +0530

    added headers into response

--- index.ts
+++ index.ts
@@ -25,6 +25,26 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://full-stack-project-hardik.netlify.app"
+  );
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  next();
+});
+
 // Routes
 app.use("/api/upload", uploadRoutes); // Use the routes for Uploading
 app.use("/api/fetch", fetchRoute); // Use the Route for Fetching
